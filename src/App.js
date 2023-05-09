2d97e780de93c0d076726c1513fffdaa34f1c07a
ahmet <ahmet55uluisik05@gmail.com>
2023-05-09 15:58:15 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import IncomeForm from "./components/IncomeForm";
@@ -7,10 +7,34 @@ function App() {
   const [income, setIncome] = useState([]);
   const [totalIncome, setTotalIncome] = useState(0);
 
+  useEffect(() => {
+    let total = 0;
+    for (let i = 0; i < income.length; i++) {
+      total += parseInt(income[i].price);
+    }
+    setTotalIncome(total);
+  }, [income]);
+
+  function deleteItem(id) {
+    const newArr = income.filter((item) => item.id !== id);
+    setIncome(newArr);
+  }
   return (
     <div className="App">
       <Header totalIncome={totalIncome} />
       <IncomeForm income={income} setIncome={setIncome} />
+      <div className="incomeListContainer">
+        {income.map((item) => {
+          return (
+            <div key={item.id} className="incomeList">
+              <div>{item.desc}</div>
+              <div>{item.price}</div>
+              <div>{item.date}</div>
+              <button onClick={(e) => deleteItem(item.id)}>delete</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/IncomeForm.js
+++ src/components/IncomeForm.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const IncomeForm = () => {
-  return <div>IncomeForm</div>;
+const IncomeForm = ({ income, setIncome }) => {
+  const desc = useRef(null);
+  const date = useRef(null);
+  const price = useRef(null);
+
+  const AddInCome = (e) => {
+    e.preventDefault();
+    setIncome([
+      ...income,
+      {
+        id: Math.floor(Math.random() * 1000),
+        desc: desc.current.value,
+        date: date.current.value,
+        price: price.current.value,
+      },
+    ]);
+    desc.current.value = "";
+    date.current.value = "";
+    price.current.value = "";
+  };
+
+  return (
+    <form className="income-form" onSubmit={AddInCome}>
+      <div className="form-inner">
+        <input
+          type="text"
+          name="desc"
+          id="desc"
+          placeholder="Income Description..."
+          ref={desc}
+        />
+        <input
+          type="number"
+          name="price"
+          id="price"
+          placeholder="Price..."
+          ref={price}
+        />
+        <input
+          type="date"
+          name="date"
+          id="date"
+          placeholder="Income Date..."
+          ref={date}
+        />
+        <input className="btn" type="submit" value="Add Income" />
+      </div>
+    </form>
+  );
 };
 
 export default IncomeForm;
